c3a5f5522ef598a7c4ff1b11cdbaa207d0164287
Sebastian Muszynski <basti@linkt.de>
2025-07-07 17:18:31 +0200

    Drop wrong device class of Ah sensors

--- components/lolan_bms_ble/sensor.py
+++ components/lolan_bms_ble/sensor.py
@@ -372,14 +372,14 @@ CONFIG_SCHEMA = cv.Schema(
             unit_of_measurement=UNIT_AMPERE_HOURS,
             icon=ICON_TOTAL_CHARGED_CAPACITY,
             accuracy_decimals=3,
-            device_class=DEVICE_CLASS_ENERGY,
+            device_class=DEVICE_CLASS_EMPTY,
             state_class=STATE_CLASS_TOTAL_INCREASING,
         ),
         cv.Optional(CONF_TOTAL_DISCHARGED_CAPACITY): sensor.sensor_schema(
             unit_of_measurement=UNIT_AMPERE_HOURS,
             icon=ICON_TOTAL_DISCHARGED_CAPACITY,
             accuracy_decimals=3,
-            device_class=DEVICE_CLASS_ENERGY,
+            device_class=DEVICE_CLASS_EMPTY,
             state_class=STATE_CLASS_TOTAL_INCREASING,
         ),
     }
